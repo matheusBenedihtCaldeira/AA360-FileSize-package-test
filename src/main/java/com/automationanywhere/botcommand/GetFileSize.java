534de871191af81d4d326a35833b1e3dddb3d72b
bene <matheusbenediht@gmail.com>
2023-07-25 08:23:01 -0300

    Get file size details action

--- src/main/java/com/automationanywhere/botcommand/GetFileSize.java
+++ src/main/java/com/automationanywhere/botcommand/GetFileSize.java
@@ -1,7 +1,19 @@
 package com.automationanywhere.botcommand;
 
+import com.automationanywhere.botcommand.data.impl.NumberValue;
+import com.automationanywhere.botcommand.exception.BotCommandException;
 import com.automationanywhere.commandsdk.annotations.BotCommand;
 import com.automationanywhere.commandsdk.annotations.CommandPkg;
+import com.automationanywhere.commandsdk.annotations.Idx;
+import com.automationanywhere.commandsdk.annotations.Pkg;
+import com.automationanywhere.commandsdk.annotations.rules.NotEmpty;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static com.automationanywhere.commandsdk.model.AttributeType.FILE;
 import static com.automationanywhere.commandsdk.model.DataType.NUMBER;
 
 
@@ -17,4 +29,32 @@ import static com.automationanywhere.commandsdk.model.DataType.NUMBER;
         return_description = "[[GetFileDetails.return_label_description]]"
 )
 public class GetFileSize {
+
+    public NumberValue action(
+            //Idx 1 would be displayed first, with a text box for entering the value.
+            @Idx(index = "1", type = FILE)
+            //UI labels.
+            @Pkg(label = "[[GetFileDetails.filePath.label]]")
+            //Ensure that a validation error is thrown when the value is null.
+            @NotEmpty
+            String filePath
+    ){
+        //Internal validation, to disallow empty strings. No null check needed as we have NotEmpty on firstString.
+        if ("".equals(filePath.trim()))
+            throw new BotCommandException("Please make sure to select a valid file");
+
+        //Business logic
+        Path fullPathOfFile;
+        Double result;
+        try{
+            fullPathOfFile = Paths.get(filePath);
+            BasicFileAttributes attributes = Files.readAttributes(fullPathOfFile, BasicFileAttributes.class);
+            result = Double.valueOf(attributes.size());
+        }catch (Exception e){
+            throw new BotCommandException(e.toString());
+        }
+
+        //Return NumberValue
+        return new NumberValue(result);
+    }
 }
